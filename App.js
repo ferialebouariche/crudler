9d627ba87f24b881ff0d73c8a8b1365547507942
Feriale Bouariche <ferial.fifi063@gmail.com>
2025-10-16 12:34:56 +0100

    CRDUL screen

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-const App = () =>   (
-    <View style={styles.container}>
-      <Text>My name is feriale Bouariche </Text>
-      <StatusBar style="auto" />
-    </View>
+import{NavigationContainer} from "@react-navigation/native";
+import{createNativeStackNavigator} from "@react-navigation/native-stack";
+import ModuleListScreen from "./src/components/screens/ModuleViewScreen";
+import ModuleAddScreen from "./src/components/screens/ModuleAddScreen";
+import ModuleModifyScreen from "./src/components/screens/ModuleModifyScreen";
+import ModuleViewScreen from "./src/components/screens/ModuleViewScreen";
+const Stack = createNativeStackNavigator();
+const App = () =>   {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator 
+         initialRouteName="ModuleListScreen"
+         screenOptions={{
+          headerStyle:{backgroundColor: "black"},
+          headerTintColor: "white",
+         }}
+         
+         >
+        <Stack.Screen
+          name="ModuleListScreen"
+          component={ModuleListScreen}
+          options={{title: "List modules"}}
+        />
+        <Stack.Screen
+          name="ModuleAddScreen"
+          component={ModuleAddScreen}
+          options={{title: " Add modules"}}
+        />
+        <Stack.Screen
+          name="ModuleModifyScreen"
+          component={ModuleModifyScreen}
+          options={{title: "Modify modules"}}
+        />
+        <Stack.Screen
+          name="ModuleViewScreen"
+          component={ModuleViewScreen}
+          options={{title: "View modules"}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
+};
   export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
