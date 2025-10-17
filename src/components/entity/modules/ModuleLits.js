44fcc4ada223281ee8e4d675e13489c8110c8582
Feriale Bouariche <ferial.fifi063@gmail.com>
2025-10-17 14:55:51 +0100

    populating module list

---
+++ src/components/entity/modules/ModuleLits.js
@@ -0,0 +1,20 @@
+import {StyleSheet, View } from "react-native";
+import ModuleItem from "./ModuleItem";
+const ModuleList = ({modules, onSelect}) => {
+    //Handlers---------------------
+    
+    //view----------------
+    return( 
+        <View style= {styles.container}>
+        {
+            modules.map((module) =>{
+                return(
+                    <ModuleItem key ={module.ModuleCode} module={module} onSelect = {onSelect}/>
+                );
+            })
+        }
+        </View>    
+    );
+};
+const styles = StyleSheet.create({});
+export default ModuleList;
