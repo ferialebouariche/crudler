c2b9a1ba8c61bdfafaa83d0d4865c723e16e3482
ferialebouariche <k2256662@kingston.ac.uk>
2025-10-23 14:19:12 +0100

    populating the moduleViewScreen (#4)
    Co-authored-by: Feriale Bouariche <ferial.fifi063@gmail.com>

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -4,16 +4,18 @@ import Screen from '../layout/Screen';
 import initialeModules from "../../data/modules.js"
 import ModuleList from '../entity/modules/ModuleLits.js';
 import RenderCount from '../UI/RenderCount.js';
-const ModuleListScreen = () =>   {
+const ModuleListScreen = ({navigation}) =>   {
     //state ---------------
     const [modules, setModules] = useState(initialeModules);
     //handler------------------
-    const handDelete = (module) =>  setModules(modules.filter((item) => item.ModuleID !== module.ModuleID));
+      const handleSelect = (module) => navigation.navigate("ModuleViewScreen", {module});
+      const handDelete = (module) =>  
+      setModules(modules.filter((item) => item.ModuleID !== module.ModuleID));
      //view--------------
   return(
     <Screen>
       <RenderCount/>
-        <ModuleList modules = {modules} onSelect ={handDelete}/>
+        <ModuleList modules = {modules} onSelect ={handleSelect}/>
         
     </Screen>
   );   
