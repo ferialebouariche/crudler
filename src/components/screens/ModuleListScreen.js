8a1da97cb1b7a83336184e00de7cdcb814bf0f93
ferialebouariche <k2256662@kingston.ac.uk>
2025-10-30 11:55:27 +0100

    Deleting the CRUDL flow (#6)
    Co-authored-by: Feriale Bouariche <ferial.fifi063@gmail.com>

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,27 +1,39 @@
-import { useState } from 'react';
-import {StyleSheet} from 'react-native';
-import Screen from '../layout/Screen';
-import initialeModules from "../../data/modules.js"
-import ModuleList from '../entity/modules/ModuleLits.js';
-import RenderCount from '../UI/RenderCount.js';
-const ModuleListScreen = ({navigation}) =>   {
-    //state ---------------
-    const [modules, setModules] = useState(initialeModules);
-    //handler------------------
-      const handleSelect = (module) => navigation.navigate("ModuleViewScreen", {module});
-      const handDelete = (module) =>  
-      setModules(modules.filter((item) => item.ModuleID !== module.ModuleID));
-     //view--------------
-  return(
+import { useState } from "react";
+import { LogBox, StyleSheet } from "react-native";
+import Screen from "../layout/Screen";
+import initialeModules from "../../data/modules.js";
+import ModuleList from "../entity/modules/ModuleLits.js";
+import RenderCount from "../UI/RenderCount.js";
+const ModuleListScreen = ({ navigation }) => {
+  //initialisation
+  LogBox.ignoreLogs([
+    "Non-serializable values were found in the navigation state",
+  ]);
+
+  //state ---------------
+  const [modules, setModules] = useState(initialeModules);
+  //handler------------------
+  const handleDelete = (module) =>
+    setModules(modules.filter((item) => item.ModuleID !== module.ModuleID));
+
+  const onDelete = (module) => {
+    handleDelete(module);
+    navigation.goBack();
+  };
+
+  const handleSelect = (module) =>
+    navigation.navigate("ModuleViewScreen", { module, onDelete });
+
+  //view--------------
+  return (
     <Screen>
-      <RenderCount/>
-        <ModuleList modules = {modules} onSelect ={handleSelect}/>
-        
+      <RenderCount />
+      <ModuleList modules={modules} onSelect={handleSelect} />
     </Screen>
-  );   
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {},
+  container: {},
 });
 export default ModuleListScreen;
