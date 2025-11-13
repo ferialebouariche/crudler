e99a86a40feb668791893c898cdc5f92d3bf914a
Feriale Bouariche <ferial.fifi063@gmail.com>
2025-11-13 13:50:38 +0100

    Adding in the CRUDL workflow

--- src/components/entity/modules/ModuleLits.js
+++ src/components/entity/modules/ModuleLits.js
@@ -1,20 +1,22 @@
-import {StyleSheet, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import ModuleItem from "./ModuleItem";
-const ModuleList = ({modules, onSelect}) => {
-    //Handlers---------------------
-    
-    //view----------------
-    return( 
-        <View style= {styles.container}>
-        {
-            modules.map((module) =>{
-                return(
-                    <ModuleItem key ={module.ModuleCode} module={module} onSelect = {onSelect}/>
-                );
-            })
-        }
-        </View>    
-    );
+const ModuleList = ({ modules, onSelect }) => {
+  //Handlers---------------------
+
+  //view----------------
+  return (
+    <View style={styles.container}>
+      {modules.map((module) => {
+        return (
+          <ModuleItem
+            key={module.ModuleCode}
+            module={module}
+            onSelect={onSelect}
+          />
+        );
+      })}
+    </View>
+  );
 };
 const styles = StyleSheet.create({});
 export default ModuleList;
